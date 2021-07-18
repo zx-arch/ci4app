f51cc457e707d1bfe1beedf40a17e303ca76c656
zx-arch <renaldi.kristian@ti.ukdw.ac.id>
2021-07-18 21:57:56 +0700

    ubah halaman home dan get data foreach

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,48 +1,58 @@
-<?php $this->extend("layout/template"); ?>
-<?php $this->section("content"); ?>
-<div class="topnav">
-    <a class="active" href="/">Home</a>
-    <a href="/kategori/terpopuler">Terpopuler</a>
-    <a href="/kategori/terbaru">Terbaru</a>
-    <a href="/status/tamat">Tamat</a>
-    <a href="/status/belumtamat">Belum Tamat</a>
-    <?php if ($hasil === "caritidakditemukan") : ?>
-        <form method="post" id="form">
-            <input type="search" name="search" value="<?= $search; ?>" autocomplete="off" id="search" class="form-control" title="Cari Minimal 4 huruf" />
-        </form>
-    <?php elseif ($hasil === "cariditemukan") : ?>
-        <form method="post" id="form">
-            <input type="search" name="search" autocomplete="off" id="search" class="form-control habiscari" value="<?= $search; ?>" title="Cari Minimal 4 huruf" />
-        </form>
-    <?php elseif ($hasil === "tidakcaridata") : ?>
-        <form method="post" id="form">
-            <input type="search" name="search" placeholder="Cari Komik Manga ..." autocomplete="off" id="search" class="form-control habiscari" title="Cari Minimal 4 huruf" />
-        </form>
-    <?php endif; ?>
-</div>
-<div class="cols">
-    <?php if (!empty($home)) : ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive CSS Card Layout using Flexbox</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/style.css'); ?>">
+</head>
+
+<body>
+    <div class="topnav">
+        <a class="active" href="/">Home</a>
+        <a href="/kategori/terpopuler">Terpopuler</a>
+        <a href="/kategori/terbaru">Terbaru</a>
+        <a href="/status/tamat">Tamat</a>
+        <a href="/status/belumtamat">Belum Tamat</a>
+        <?php if ($hasil === "caritidakditemukan") : ?>
+            <form method="post" id="form">
+                <input type="search" name="search" value="<?= $search; ?>" autocomplete="off" id="search" class="form-control" title="Cari Minimal 4 huruf" />
+            </form>
+        <?php elseif ($hasil === "cariditemukan") : ?>
+            <form method="post" id="form">
+                <input type="search" name="search" autocomplete="off" id="search" class="form-control habiscari" value="<?= $search; ?>" title="Cari Minimal 4 huruf" />
+            </form>
+        <?php elseif ($hasil === "tidakcaridata") : ?>
+            <form method="post" id="form">
+                <input type="search" name="search" placeholder="Cari Komik Manga ..." autocomplete="off" id="search" class="form-control habiscari" title="Cari Minimal 4 huruf" />
+            </form>
+        <?php endif; ?>
+    </div>
+    <!--card layout start-->
+    <div class="cards-list">
+        <?php $count = 0; ?>
         <?php foreach ($home as $data) : ?>
-            <div class="col" ontouchstart="this.classList.toggle('hover');">
-                <div class="container">
-                    <div class="front" style="background-image: url(<?= $data['sampul']; ?>)">
-                        <div class="inner">
-                            <span style="font-weight: bold;"><?= $data['judul']; ?></span>
-                        </div>
-                    </div>
-                    <div class="back">
-                        <div class="inner">
-                            <p style="color: black;">Komik ini ditulis oleh <?= $data['penulis']; ?>, dirilis pada tahun <?= $data['tahun_rilis']; ?>, dengan rating <?= $data['rating']; ?></p>
-                            <a href="<?= $data['slug']; ?>" id="tombolbacakomik" class="button6">Baca Komik</a>
-                        </div>
+            <?php $count++; ?>
+            <?php if (fmod($count, 6) > 0) : ?>
+                <div class="card 1">
+                    <div class="card_image" onclick='foo(event, "<?= $data['judul']; ?>");''>
+                        <img src="<?= $data['sampul']; ?>" />
+                        <input type="hidden" name="<?= $data['judul']; ?>" class="data">
                     </div>
                 </div>
-            </div>
+            <?php else : ?>
+                <?php continue; ?>
+            <?php endif; ?>
         <?php endforeach; ?>
-    <?php endif; ?>
-</div>
-<?php if (empty($home)) : ?>
-    <h2 style="text-align: center;color:white;">Komik "<?= $search; ?>" Belum Tersedia</h2>
-<?php endif; ?>
+    </div>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url('js/script.js'); ?>"></script>
 
-<?php $this->endSection(); ?>
+
+    <!--card layout end-->
+
+</body>
+
+</html>
--- public/js/script.js
+++ public/js/script.js
@@ -6,5 +6,30 @@ $('#search').bind('keyup', function() {
         $('#form').submit();
     }
 });
+function foo(event,data) {
+    console.log(data);
+}
+// $(".card_image").click(function(){
+//     alert("Value: " + $(".naruto").text());
+// });
+// $(".card_image").click(function() {
+//     Swal.fire({
+//         title: '<strong>HTML <u>example</u></strong>',
+//         icon: 'info',
+//         html:
+//             'You can use <b>bold text</b>, ' +
+//             '<a href="//sweetalert2.github.io">links</a> ' +
+//             'and other HTML tags',
+//         showCloseButton: true,
+//         showCancelButton: true,
+//         focusConfirm: false,
+//         confirmButtonText:
+//             '<i class="fa fa-thumbs-up"></i> Great!',
+//         confirmButtonAriaLabel: 'Thumbs up, great!',
+//         cancelButtonText:
+//             '<i class="fa fa-thumbs-down"></i>',
+//         cancelButtonAriaLabel: 'Thumbs down'
+//     })
+// });
 
 
