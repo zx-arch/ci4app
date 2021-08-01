432c49609dedad1bc82a6611d283ec12ebd5fcf4
zx-arch <renaldi.kristian@ti.ukdw.ac.id>
2021-08-01 11:49:05 +0700

    fix bug responsive home page dan edit file css js
    ketika html punya file css dan js yang terpisah dari file html, itu memungkinkan untuk di inspect dan dilihat isinya, dengan menggabungkan isi file css dan js menjadi 1 baris itu tujuannya supaya user tidak mudah mengetahui struktur isi file css dan js nya. Seperti misalnya kalau kita lihat isi file dari framework bootstrap itu  juga banyak script cssnya yang digabung 1 baris

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,1 @@
-var elm = document.forms['form'].elements['search'];
-elm.focus();
-elm.setSelectionRange(elm.value.length,elm.value.length);
-$('#search').bind('keyup', function() {
-    if ($(this).val().length >= 1) {
-        $('#form').submit();
-    }
-});
+const elm = document.forms['form'].elements['search'];elm.focus();elm.setSelectionRange(elm.value.length,elm.value.length);$('#search').bind('keyup', function() {if ($(this).val().length >= 1) {$('#form').submit();}});
